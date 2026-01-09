89ac09243320cfa3bb44e790a843119579604cbd
nehagarade <111562792+nehagarade@users.noreply.github.com>
2026-01-09 22:34:55 +0530

    Refactor variable.tf by removing unused variables
    Removed unused variables and cleaned up variable definitions.

--- variable.tf
+++ variable.tf
@@ -1,30 +1,8 @@
-variable "bucket_name" {
-  description = "Globally unique name for the S3 bucket"
-  type        = string
-}
-
 variable "aws_region" {
-  description = "AWS region where the S3 bucket will be created"
-  type        = string
-  default     = "us-east-1"
+  type    = string
+  default = "us-east-1"
 }
 
-variable "environment" {
-  description = "Environment name (dev, qa, prod)"
-  type        = string
-  default     = "dev"
-}
-
-variable "enable_versioning" {
-  description = "Enable S3 bucket versioning"
-  type        = bool
-  default     = true
-}
-
-variable "tags" {
-  description = "Tags to apply to the S3 bucket"
-  type        = map(string)
-  default = {
-    Project = "terraform-s3"
-  }
+variable "bucket_name" {
+  type = string
 }
